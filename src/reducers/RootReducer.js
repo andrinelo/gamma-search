c9d534d1083fbc9225987c5ccb2296c33894665d
Maria Nord-Varhaug <mnordvh@gmail.com>
2020-09-22 14:24:57 +0200

    #7:forgot to add rootreducer to staged changes :(

--- src/reducers/RootReducer.js
+++ src/reducers/RootReducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 import testReducer from "./TestReducer.js";
+import windowReducer from "./WindowReducer.js";
 
 // Combines all reducers and export them
 export default combineReducers({
   testVariable: testReducer,
+  activeWindow: windowReducer
 });
