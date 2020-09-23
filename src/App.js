61a925c17e22fc55636443b613061475837fc24d
Tor Martin Wang <tmwang@stud.ntnu.no>
2020-09-23 12:59:15 +0200

    Added dispatching of the fetch, in App.js

--- src/App.js
+++ src/App.js
@@ -3,16 +3,31 @@ import './App.css';
 import GremlinQueryDisplayAccordion from './components/GremlinQueryDisplayAccordion';
 import OutputAccordion from './components/OutputAccordion'
 import CloudButton from './components/CloudButton'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchFullResults } from "./actions/FetchResultsActions.js";
 
 function App() {
-  return (
-        <div>
-          <GremlinQueryDisplayAccordion></GremlinQueryDisplayAccordion>
-          <OutputAccordion>
-          </OutputAccordion>
-          <CloudButton></CloudButton>
-        </div>
 
+  // Fetches gremlinquery from Redux store
+  const gremlinQuery = useSelector(state => state.gremlinQuery)
+  
+  // Could also just define gremlinQuery manually like this:
+  //const gremlinQuery = "g.V().hasLabel('Person')"
+
+  // Dispatcher used to dispatch actions in Redux
+  const dispatch = useDispatch()
+  
+
+  // Dispatches action to fetch results from Ardoq-API using the above gremlinquery
+  dispatch(fetchFullResults(gremlinQuery))
+
+  return (
+    <div>
+      <GremlinQueryDisplayAccordion></GremlinQueryDisplayAccordion>
+      <OutputAccordion>
+      </OutputAccordion>
+      <CloudButton></CloudButton>
+    </div>
   );
 }
 
--- src/actions/FetchResultsActions.js
+++ src/actions/FetchResultsActions.js
@@ -2,12 +2,13 @@ import { FETCH_FULL_RESULTS } from "./types.js";
 
 // Redux actions.
 
-export function setFullResults(gremlinQuery) {
+export function fetchFullResults(gremlinQuery) {
 
   // Write fetch-code here
 
+  // Alerts just to check that the gremlin query is correct. Delete this.
+  alert(gremlinQuery)
 
-  
   return {
     results: "", // Set results here Torstein
     type: FETCH_FULL_RESULTS
