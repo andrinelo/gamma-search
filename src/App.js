2b8be8b605ccd29336074713c071abfbcd0e1bfd
atoft97 <asbjotof@stud.ntnu.no>
2020-09-24 09:43:05 +0200

    minor changes

--- src/App.js
+++ src/App.js
@@ -4,23 +4,19 @@ import GremlinQueryDisplayAccordion from './components/GremlinQueryDisplayAccord
 import OutputAccordion from './components/OutputAccordion'
 import CloudButton from './components/CloudButton'
 import StartNodeInputField from './components/StartNodeInputField.js'
-
 import RelationButton from "./components/RelationButton"
-/*
-<GremlinQueryDisplayAccordion></GremlinQueryDisplayAccordion>
-      <OutputAccordion>
-      </OutputAccordion>
-      <CloudButton></CloudButton>
-*/
 function App() {
+
   return (
-
     <div>
-      <h1>Start of the app :)</h1>
-      <RelationButton edgeId = {1}></RelationButton>
-      <RelationButton edgeId = {2}></RelationButton>
+        <GremlinQueryDisplayAccordion></GremlinQueryDisplayAccordion>
+        <OutputAccordion>
+        </OutputAccordion>
+        <CloudButton></CloudButton>
+        <StartNodeInputField></StartNodeInputField>
 
-
+        <RelationButton edgeId = {1}></RelationButton>
+        <RelationButton edgeId = {2}></RelationButton>
       
     </div>
 
