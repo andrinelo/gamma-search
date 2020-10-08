5f004053c4c5fcbffed355d24547db8f222866ed
Jon Oddvar Rambjør <44160493+jonoddram@users.noreply.github.com>
2020-10-08 09:07:42 +0200

    Issue #68: Output field text is now human readable + displays image if json contains image url + always shows label and name if exists first

--- src/components/OutputAccordion.js
+++ src/components/OutputAccordion.js
@@ -19,12 +19,22 @@ const useStyles = makeStyles({
 
 const outputsPerPage = 4
 export default function OutputAccordion(props) {
-    const indexValue = useSelector(state => state.currentOutputPage)
-    let textList = useSelector(state => state.allQueryResults[FULL_RESULT_ITEMS])
-    textList = textList.map(item => JSON.stringify(item))
+    const indexValue = useSelector(state => state.currentOutputPage);
+    const textList = useSelector(state => state.allQueryResults[FULL_RESULT_ITEMS]);
     const classes = useStyles();
-    const showList = textList.slice(indexValue*outputsPerPage, Math.min(indexValue*outputsPerPage + outputsPerPage, textList.length+1))
-    const pageCount = textList.length % outputsPerPage == 0 ? textList.length/outputsPerPage : Math.floor(textList.length/outputsPerPage) + 1;
+    // Find the subset of the list that will be displayed
+    const shortenedList = textList.slice(indexValue*outputsPerPage, Math.min(indexValue*outputsPerPage + outputsPerPage, textList.length+1));
+    // Parse the subset of the list
+    const showList = shortenedList.map(item => OutputJSONParser(item));
+    // Find image urls for image display
+    const imageURLList = shortenedList.map(item => GetUrl(item));
+    // Twin together the lists
+    const dataImageList = [];
+    for (let i0 = 0; i0 < imageURLList.length; i0++) {
+        dataImageList.push([showList[i0], imageURLList[i0]]);
+    }
+    // Determine page count
+    const pageCount = textList.length % outputsPerPage == 0 ? textList.length/outputsPerPage : Math.floor(textList.length/outputsPerPage);
     return (
         <Accordion class={classes}>
             <AccordionSummary
@@ -34,9 +44,9 @@ export default function OutputAccordion(props) {
             </AccordionSummary>
             <AccordionDetails>
                 <Container>
-                    {showList.map((text) => 
+                    {dataImageList.map((values) => // Map images too
                             <Container class={classes.OutputListElementContainer}>
-                                <OutputListElement text={text}></OutputListElement>
+                                <OutputListElement values={values} ></OutputListElement>
                             </Container>)}
                     <OutputPaginator pageCount={pageCount}></OutputPaginator>
                 </Container>
@@ -44,3 +54,58 @@ export default function OutputAccordion(props) {
         </Accordion>
     );
 }
+
+
+function OutputJSONParser(inputJSON) {
+    const toReturn = [];
+    if ("properties" in inputJSON && "label" in inputJSON) { // If this is not the case toReturn is empty
+        toReturn.push({"property":"label", "value":inputJSON["label"]}); // Start by adding label to the front
+        if ("name" in inputJSON["properties"]) { 
+            if (Array.isArray(inputJSON["properties"]["name"])) { // if name is a tuple (id, string)
+                if (0 in inputJSON["properties"]["name"]) { // List containing a tuple
+                    toReturn.push({"property":"name", "value":inputJSON["properties"]["name"][0]["value"]})
+                }
+                else if ("value" in inputJSON["properties"]["name"]) { 
+                    toReturn.push({"property":"name", "value":inputJSON["properties"]["name"]["value"]})
+                }
+            }
+            else { 
+                toReturn.push({"property":"name", "value":inputJSON["properties"]["name"]}); // If there is a name property add it to the front
+            }
+        }
+        if ("id" in inputJSON) {
+            toReturn.push({"property":"id", "value":inputJSON["id"]});
+        }
+        for (const property in inputJSON["properties"]) { // for all the properties possible
+            if (property != "name") { // Name property already added
+                if (Array.isArray(inputJSON["properties"][property])) { // If property value is formated as an array
+                    if (0 in inputJSON["properties"][property]) { // List containing a tuple
+                        toReturn.push({"property":property, "value":inputJSON["properties"][property][0]["value"]});
+                    }
+                    else if ("value" in inputJSON["properties"][property]) { // and if property value array contains field "value"
+                        toReturn.push({"property":property, "value":inputJSON["properties"][property]["value"]});
+                    }
+                }
+                else { //Else the value is easily accessible
+                toReturn.push({"property":property, "value":inputJSON["properties"][property]});
+                }
+            }
+        }
+    }
+    return toReturn;
+  }
+
+  function GetUrl(inputJSON) {
+      if ("properties" in inputJSON && "image" in inputJSON["properties"]) {
+        if (0 in inputJSON["properties"]["image"]) { // List containing a tuple
+            return inputJSON["properties"]["image"][0]["value"];
+        }
+        else if ("value" in inputJSON["properties"]["image"]) { // and if property value array contains field "value"
+            return inputJSON["properties"]["image"]["value"];
+        }
+        else {
+            return inputJSON["properties"]["image"]
+        }
+      }
+      return null   
+  }
