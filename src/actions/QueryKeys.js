e3a99449466095b44bb8f668bb6d527d91c112e3
Tor Martin Wang <tmwang@stud.ntnu.no>
2020-10-14 20:39:56 +0200

    Properties that only have numbers as values in the database are now interpreted as numbers when filtering on them; some properties only has numbers saved as strings as values, and filtering on those properties does not work at the moment. Also made severeal QOL-improvements; for eexample when removing all filterlines and saving, the next time you open the filter menu a new empty filterline is automatically created for you. When you now change the chosen property the value field is reset.

--- src/actions/QueryKeys.js
+++ src/actions/QueryKeys.js
@@ -3,5 +3,5 @@ export const ALL_AVAILABLE_LABELS = "allAvailableLabels";
 export const INSPECTED_EDGES_IN_DATASET = "inspectedEdgesInDataset";
 export const INSPECTED_NODES_IN_DATASET = "inspectedNodesInDataset";
 export const ALL_PROPERTIES_OF_DATASET = "allPropertiesOfDataset";
-export const VALUES_FOR_PROPERTY_IN_DATASET = "allValuesForProperty-";
+export const VALUES_FOR_PROPERTY_IN_DATASET = "allFilterValuesForProperty-";
 
