cb473ef333d735b36092ecc6fd95d372db4dfea2
atoft97 <atoft97@gmail.com>
2020-10-19 01:45:23 +0200

    minor changes

--- src/reducers/FilterReducer.js
+++ src/reducers/FilterReducer.js
@@ -4,10 +4,6 @@ import { SET_FILTER } from "./../actions/types.js";
 const filterReducer = (state = {}, action) => {
   switch (action.type) {
     case SET_FILTER:
-      //let filter = { ...state, [action.cloudId]: action.value  };
-      console.log(action.andOrs)
-      console.log(action.value)
-
       let combinedObject = Object.assign(action.value, action.andOrs); 
 
       let filter = { ...state, [action.cloudId]: combinedObject};
