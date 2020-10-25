2be173379a3382828bf88a851a9e68ce78a29df7
Tor Martin Wang <tmwang@stud.ntnu.no>
2020-10-25 19:27:03 +0100

    The autocomplete for values in the filter menu now works again

--- src/components/FilterMenu.js
+++ src/components/FilterMenu.js
@@ -49,7 +49,10 @@ function FilterMenu(props) {
   const stateFilters = useSelector((state) => state.filters);
   const dispatch = useDispatch()
   const classes = useStyles();
-
+  
+  // The following two values are only used to force a component re-render when the autocomplete-values for the selected property are retrieved
+  const [latestSelectedProperty, setLatestSelectedProperty] = useState("")
+  const latestPropertyValues = useSelector(state => latestSelectedProperty !== "" ? state.allQueryResults[DATASET_PROPERTY_VALUES_BEFORE_DATASET_FILTERS + latestSelectedProperty] : [])
   
   // Gremlin query corresponding to the gremlin query for the selected dataset, ignoring any filters on this particular dataset
   let datasetBeforeFiltersGremlinQuery = useSelector(store => store.gremlinQueryParts.slice(0, 1 + selectedDataset * 2).join(""))
@@ -57,6 +60,7 @@ function FilterMenu(props) {
 
   // Fetches all possible labels, to be used as auto-suggestions
   const allProperties = useSelector(state => state.allQueryResults[DATASET_PROPERTIES_BEFORE_DATASET_FILTERS])
+  
   
   // Initializing menu
   const [localFilters, setLocalFilters] = useState([
@@ -66,8 +70,9 @@ function FilterMenu(props) {
       value: "",
     },
   ], [selectedDataset]);
-
+  
   const [andOrs, setAndOrs] = useState([], [selectedDataset]);
+
 
   let [shouldSetFiltersFromStore, setshouldSetFiltersFromStore] = useState(true)
 
@@ -156,6 +161,7 @@ function FilterMenu(props) {
         localFilters[index]['operator'] = "=="
     }
 
+    setLatestSelectedProperty(selectedProperty)
     setLocalFilters([...localFilters]);
   };
 
