20f5512fdd0fc907d568586d1c6a50dd18867c88
Tor Martin Wang <tmwang@stud.ntnu.no>
2020-10-27 11:28:29 +0100

    Added fade in and out on scroll on first info-row

--- src/components/InfoContainer.js
+++ src/components/InfoContainer.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import 'fontsource-roboto';
 import Typography from '@material-ui/core/Typography';
-import Grow from '@material-ui/core/Grow';
+import Fade from '@material-ui/core/Fade';
+import VizSensor from 'react-visibility-sensor';
 
 function InfoContainer() {
 
@@ -10,24 +11,47 @@ function InfoContainer() {
   return (
 
     <div style={{display: 'flex', flexDirection: 'column', width: '80%', margin: 'auto'}}>
-    <button onClick={() => setChecked(!checked)}></button>
       <div style={{display: 'flex', flexDirection: 'row', width: '100%'}}>
-        <div style={{ width: '50%' }}>
-          <img style={{width: '100%'}} src={'/NetworkIllustration2.jpg'} alt={'Network / Graph database'}></img>
-        </div>
-        <Grow 
-          in={checked} timeout={2000}
-          style={{ transformOrigin: '0 0 0' }}
-          {...(checked ? { timeout: 1000 } : {})}>
-          <div style={{width: '50%', textAlign: 'center'}}>
-            <Typography variant="h5" style={{marginBottom: '5px', marginTop: '15px'}}>
-              The graph database defined
-            </Typography>
-            <Typography variant="body1">
-              Graph databases are purpose-built to store and navigate relationships. Relationships are first-class citizens in graph databases, and most of the value of graph databases is derived from these relationships. Graph databases use nodes to store data entities, and edges to store relationships between entities. An edge always has a start node, end node, type, and direction, and an edge can describe parent-child relationships, actions, ownership, and the like. There is no limit to the number and kind of relationships a node can have.
-            </Typography>
-          </div>
-        </Grow>
+        
+        <VizSensor
+          onChange={(isVisible) => {
+              setChecked(isVisible);
+          }}
+        >
+          <Fade 
+            in={checked} timeout={1000}
+            style={{ transformOrigin: '0 0 0' }}
+            {...(checked ? { timeout: 1000 } : {})}
+          >
+            <div style={{ width: '50%' }}>
+              <img style={{width: '100%'}} src={'/NetworkIllustration2.jpg'} alt={'Network / Graph database'}></img>
+            </div>
+          </Fade>
+        
+        </VizSensor>
+
+        <VizSensor
+          onChange={(isVisible) => {
+              setChecked(isVisible);
+          }}
+        >
+          <Fade 
+            in={checked} timeout={1000}
+            style={{ transformOrigin: '0 0 0' }}
+            {...(checked ? { timeout: 1000 } : {})}
+          >
+            <div style={{width: '50%', textAlign: 'center'}}>
+              <Typography variant="h5" style={{marginBottom: '5px', marginTop: '15px'}}>
+                The graph database defined
+              </Typography>
+              <Typography variant="body1">
+                Graph databases are purpose-built to store and navigate relationships. Relationships are first-class citizens in graph databases, and most of the value of graph databases is derived from these relationships. Graph databases use nodes to store data entities, and edges to store relationships between entities. An edge always has a start node, end node, type, and direction, and an edge can describe parent-child relationships, actions, ownership, and the like. There is no limit to the number and kind of relationships a node can have.
+              </Typography>
+            </div>
+          </Fade>
+        
+        </VizSensor>
+
       </div>
     </div>
   )
