e81d37924d5602362830a642c83a0cc5df1bcfe0
torsteinotterlei <torstein@otterlei.no>
2020-10-29 09:48:26 +0100

    Change NTNU link adress in header

--- src/App.js
+++ src/App.js
@@ -120,7 +120,7 @@ function App() {
   return (
     <div>
       <div style={{backgroundColor: 'white', display:"flex", justifyContent:"space-between", alignItems:"center", padding:"5px 10px 5px 10px"}}>
-        <a target="_blank" rel="noopener noreferrer" href="https://www.ntnu.no/studier/emner/TDT4290/2017/1#tab=omEmnet">
+        <a target="_blank" rel="noopener noreferrer" href="https://www.ntnu.no/studier/emner/TDT4290">
           <img src="./ntnu_logo.png" alt="NTNU" style={{maxHeight:"20px", marginRight:"35px"}}/>
         </a>
         <a href="/">
